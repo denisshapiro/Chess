a892e8009d23286bf72ab5a31a0c5d29ff4461c5
denisshapiro <denis.o.shapiro@gmail.com>
2019-07-26 00:22:52 -0500

    add basic King class

--- pieces/king.rb
+++ pieces/king.rb
@@ -3,7 +3,7 @@ require File.expand_path('./helpers.rb')
 
 # Class to describe a King's available moves in current possiton
 class King < Piece
-  attr_accessor :possible_moves, :piece
+  attr_accessor :possible_moves, :piece, :moves_made
 
   def initialize(pos, color, current_board)
     @pos = pos
@@ -11,7 +11,21 @@ class King < Piece
     @piece = Helpers.corresponding_hash(@color)[:King]
     @moves_made = 0
     @current_board = current_board
-    @possible_moves = []
-    #generate_moves
+    @x_moves = [1, 1, -1, 0, -1, 0, 1, -1]
+    @y_moves = [1, 0, 0, 1, 1, -1, -1, -1]
+  end
+
+  def king_move_possible?(curr)
+    on_board?(curr) && !taken_by_same_color_piece(curr) && !taken_by_opposite_king?(curr)
+  end
+
+  def generate_moves(pos)
+    moves = []
+
+    8.times do |index|
+      curr = [pos[0] + @x_moves[index], pos[1] + @y_moves[index]]
+      moves.push([pos, curr]) if king_move_possible?(curr)
+    end
+    moves
   end
 end
